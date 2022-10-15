d3a90f3e64698ccfeb6c87626ccae9df2c3e977d
plasmapotential <thomaslooby@gmail.com>
2022-10-15 08:30:37 -0400

    Lmatrix

--- radPowerTools/Lmatrix3D_v2.py
+++ radPowerTools/Lmatrix3D_v2.py
@@ -38,40 +38,45 @@ uvw2xyz = np.vstack([u,v,w])
 #print(u)
 #print(v)
 #print(w)
-print(uvw2xyz)
+#print(uvw2xyz)
 
 #discretize the half hemisphere along w in bin centers
-pdf_a = lambda x: np.sin(x)/2.0
+pdf_a = lambda x: np.sin(x)/2.0 #normalized angle pdf
 a = np.linspace(0,np.pi,100000)
 alpha = pdf_a(a)
 cdf_a = np.cumsum(alpha[1:])*np.diff(a)
 cdf_a = np.insert(cdf_a, 0, 0)
 cdfBounds_a = np.linspace(0,cdf_a[-1],Na+1)
 cdfSlices_a = np.diff(cdfBounds_a)/2.0 + cdfBounds_a[:-1]
-inverseCDF = interp1d(cdf_a, a, kind='linear')
-aSlices = inverseCDF(cdfSlices_a)
+inverseCDF_a = interp1d(cdf_a, a, kind='linear')
+aSlices = inverseCDF_a(cdfSlices_a)
+aBounds = inverseCDF_a(cdfBounds_a)
 #beta
 # alpha
-pdf_b = lambda x: [1.0 for i in x]
+pdf_b = lambda x: [1.0/np.pi for i in x] #normalized angle PDF
 b = np.linspace(0,np.pi,100000) #only half hemisphere
 beta = pdf_b(b)
 cdf_b = np.cumsum(beta[1:])*np.diff(b)
 cdf_b = np.insert(cdf_b, 0, 0)
 cdfBounds_b = np.linspace(0,cdf_b[-1],Nb+1)
 cdfSlices_b = np.diff(cdfBounds_b)/2.0 + cdfBounds_b[:-1]
-inverseCDF = interp1d(cdf_b, b, kind='linear')
-bSlices = inverseCDF(cdfSlices_b)
+inverseCDF_b = interp1d(cdf_b, b, kind='linear')
+bSlices = inverseCDF_b(cdfSlices_b)
+bBounds = inverseCDF_b(cdfBounds_b)
 
 #print(np.degrees(aSlices))
 #print(np.degrees(bSlices))
-
+print(np.degrees(aBounds))
+print(np.degrees(bBounds))
+#print(cdfBounds_a)
+#print(cdfBounds_b)
 
 
 #calculate distance from ctr to each source object
 vec = radXYZ - ctr
-
+l = np.linalg.norm(vec, axis=1)
 UVW = np.matmul(vec, uvw2xyz.T)
-l = np.linalg.norm(UVW, axis=1)
+
 #calculate which half hemisphere cell the ray from ctr to each rad pt is in
 angles = np.zeros((Ni,Nj,2))
 for i in range(Ni):
@@ -82,17 +87,33 @@ for i in range(Ni):
     angles[i,:,0] = np.round(np.arcsin(w0), 8)
     #beta
     angles[i,:,1] = np.round(np.arcsin( v0 / np.cos(angles[i,:,0]) ), 8)
-
-
 #print(angles)
 #print(np.degrees(angles))
 
-#YOU ARE HERE!!!
 #now calculate L matrix
 L = np.zeros((Ni,Nj))
 for i in range(Ni):
     for j in range(Nj):
-        #check if ray is inside this j
+        #alpha
+        for k in range(len(aBounds)-1):
+            #check if ray is inside this j
+            if (angles[i,j,0] >= aBounds[k]) and (angles[i,j,0] <= aBounds[k+1]):
+                aFlag = True
+            else:
+                aFlag = False
+        #beta
+        for k in range(len(bBounds)-1):
+            #check if ray is inside this j
+            if (angles[i,j,1] >= bBounds[k]) and (angles[i,j,1] <= bBounds[k+1]):
+                bFlag = True
+            else:
+                bFlag = False
+        if aFlag == True and bFlag == True:
+            L[i,j] = l[i]
+            
+#scale to account for fractional component of solid angle
+L *= 2*np.pi / Nj
 
-        #assign distance to vector
-        L[i,j]
+print(L)
+
+#now you need to scale by fractional solid angle
