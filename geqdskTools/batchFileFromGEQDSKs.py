b2dd81945b3548dcca509421658c9731b5221ff2
plasmapotential <thomaslooby@gmail.com>
2023-11-04 11:04:29 -0400

    v4HEAT

--- geqdskTools/batchFileFromGEQDSKs.py
+++ geqdskTools/batchFileFromGEQDSKs.py
@@ -5,7 +5,11 @@
 import os
 
 #path where gfiles are read from and where we will save the batchFile.dat
-rootPath = '/home/tom/work/CFS/GEQDSKs/TSCruns/TSC-V2h01/TSC-V2h01/corrected_v2y_Ip_Bt_psi_Fpol/interpolated_100ms/'
+rootPath = '/home/tom/HEATruns/SPARC/oscillation/interpolated/dt200us_sinusoid_10mm_500Hz/'
+
+#HEAT version
+#v = 3
+v = 4
 
 #read all files with a prefix
 prefix = 'g000001'
@@ -13,9 +17,17 @@ gFileList = sorted([f for f in os.listdir(rootPath) if (os.path.isfile(os.path.j
 print(gFileList)
 input('Press any key to continue')
 for i,g in enumerate(gFileList):
-    t = int(g.split('.')[1])
-    #append line to batchFile
-    line = 'sparc,rampup_TSCvh01a, '+g+', fullSlice.stp, PFCs.csv, SPARC_input_{:05d}.csv, hfOpt:T\n'.format(t)
+    #HEAT v3.0 convention
+    if v == 3:
+        t = int(g.split('.')[1])
+        #append line to batchFile
+        line = 'sparc, oscillationTest,'+g+', T4_20230915_nominal.step, PFCs.csv, SPARC_input_{:05d}.csv, hfOpt:T\n'.format(t)
+    #HEAT v4.0 convention
+    else:
+        t = g.split('_')[1]
+        #append line to batchFile
+        line = 'sparc, 1, oscillationTest, '+t+', '+g+', T4_20230915_nominal.step, PFCs.csv, SPARC_input.csv, hfOpt:T\n'.format(t)
+    
     if i==0:
         with open(rootPath + "batchFile.dat", "w") as myfile:
             myfile.write(line)
