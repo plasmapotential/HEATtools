cc4e46b40abc03d25a2dfc65e08155663b7174a4
plasmapotential <thomaslooby@gmail.com>
2023-11-16 15:36:43 -0500

    pattern update

--- CADtools/patternPart.py
+++ CADtools/patternPart.py
@@ -1,15 +1,12 @@
 #script to pattern a part object toroidally
 #meant to be used as a macro in FreeCAD python console
 
-
-
-# User-defined variables
-partName = "torBez30mm_2mmCham"  # Replace with the name of your part
 N_tor = 3  # number of copies in toroidal direction
-N_pol = 2  # number of copies in poloidal direction
+N_pol = 3  # number of copies in poloidal direction
 translationZ = 106.5  # Translation increment for z-axis [mm]
+tSign = -1.0 #translate in positive or negative z
 rotationAngle = 2.5  # Rotation increment about z-axis [degrees]
-
+aSign = 1.0 #translate in positive or negative angle
 
 objs = FreeCADGui.Selection.getSelection() #only use first selected part
 
@@ -23,8 +20,8 @@ for i in range(N_tor):
             newObjs[-1].Placement = obj.getGlobalPlacement()
 
             # Calculate transformation
-            translation = App.Vector(0, 0, translationZ * (i + 1))
-            rotation = App.Rotation(App.Vector(0, 0, 1), rotationAngle * (j + 1))
+            translation = App.Vector(0, 0, tSign*translationZ * (i))
+            rotation = App.Rotation(App.Vector(0, 0, 1), aSign*rotationAngle * j)
 
             # Apply transformation
             newObjs[-1].Placement = App.Placement(translation, rotation)
