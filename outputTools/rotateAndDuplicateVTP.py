9c8538f9c508207e0c0ea095a352d4025a503fad
tloobyCFS <tlooby@cfs.energy>
2024-03-15 12:18:32 -0400

    added ability to offset vtp rotation tool

--- outputTools/rotateAndDuplicateVTP.py
+++ outputTools/rotateAndDuplicateVTP.py
@@ -1,18 +1,26 @@
 #copies a VTP file and then patterns it for 360degrees
 #using the N in the loop below.  must be run in an environment with 
-#pvpython (ie in the HEAT docker container)
+#pvpython (ie in the HEAT docker container), or call the pvpython <filename>.py
+#from terminal
+#example path to pvpython:
+#/home/tlooby/opt/paraview/ParaView-5.11.0-RC1-MPI-Linux-Python3.9-x86_64/bin/pvpython
+
 import paraview.simple as pvs
 
-rootPath = '/root/HEAT/data/sparc_000001_fishScale_T4_20230822/000001/paraview/'
+rootPath = '/home/tlooby/HEAT/data/sparc_000001_innerDivertorTest/0.010000000/paraview/'
 f = rootPath + 'HF_optical_all_mesh.vtp'
+print("Reading file")
 d1 = pvs.OpenDataFile(f)
+print("Done reading")
 
 transformed_datasets = []  # List to store transformed datasets
 
-for i in range(36):
+N = 9
+offset = -172.5
+for i in range(N):
     # Create a transform filter
     transformFilter = pvs.Transform(Input=d1)
-    transformFilter.Transform.Rotate = [0, 0, 10.0*(i+1)]
+    transformFilter.Transform.Rotate = [0, 0, 10.0*(i+1) + offset]
     # Update the filter to apply the transformation
     pvs.UpdatePipeline(proxy=transformFilter)
     # Append the transformed data for later combination
